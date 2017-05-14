e8755c04b34a71af09d4bed8873fff53057e7441
jason <bjason@gmail.com>
2017-05-14 14:35:31 -0700

    Day to, it continues.;

---
+++ 002/Project Day 2 - Random Word/Project Day 2 - Random Word/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project Day 2 - Random Word
+//
+//  Created by jason on 5/14/17.
+//  Copyright © 2017 jasonify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
