0768bb733e74b714d546ba16107f360c44915b89
jason <bjason@gmail.com>
2017-05-14 14:55:18 -0700

    Basic design done

--- 002/Project Day 2 - Random Word/Project Day 2 - Random Word/ViewController.swift
+++ 002/Project Day 2 - Random Word/Project Day 2 - Random Word/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var wordLabel: UILabel!
 
+    @IBOutlet weak var shakeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+ 
+        
+        // iOS Attributed Strings:
+        // https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/AttributedStrings/AttributedStrings.html
+        
+        
+        let attributes = [NSStrokeWidthAttributeName: 8.0,
+                          NSStrokeColorAttributeName: UIColor.white,
+                          NSForegroundColorAttributeName: UIColor.clear] as [String : Any]
+        
+        shakeLabel.attributedText = NSAttributedString(string: "< SHAKE >", attributes: attributes)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
