520a520521867d53fc87111fca1ffa996e60a3d4
jason <bjason@gmail.com>
2017-05-15 23:11:49 -0700

    basics

--- 003/main.js
+++ 003/main.js
@@ -4,9 +4,23 @@ $(document).ready(function(){
   $colorpicker = $("#colorpicker");
   $rslider = $("#rslider");
 
+  // Our RGB values:
+  var r = 200;
+  var g = 216;
+  var b = 247;
 
+
+  var setRGBs = function(){
+    $("body").css({
+      "background": "rgb(" + r + "," + g + "," + b + ")"
+    });
+  };
+
+
+  setRGBs();
   $rslider.on('input change', function(){
-    console.log($rslider.val());
+    r = $rslider.val();
+    setRGBs();
   });
 
 });
