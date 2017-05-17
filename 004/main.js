df5426d5c5dbf140de488a98bd5a5b2422174e04
jason <bjason@gmail.com>
2017-05-16 21:54:20 -0700

    canvas

---
+++ 004/main.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+  var canvas = document.getElementById("canvas");
+  var width = canvas.width = window.innerWidth;
+  var height = canvas.height = window.innerHeight;
+};
