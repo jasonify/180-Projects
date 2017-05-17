d118851a6efe8e71dc4f010a32d8719afbee896f
jason <bjason@gmail.com>
2017-05-16 21:57:51 -0700

    render

--- 004/main.js
+++ 004/main.js
@@ -2,4 +2,10 @@ window.onload = function(){
   var canvas = document.getElementById("canvas");
   var width = canvas.width = window.innerWidth;
   var height = canvas.height = window.innerHeight;
+
+  var render = function(){
+    requestAnimationFrame(render, 1000/60);
+  }
+
+  render();
 };
