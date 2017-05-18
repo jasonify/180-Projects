e1087549a21ab66e71ecdc44eeb071a79681d871
jason <bjason@gmail.com>
2017-05-17 22:55:47 -0700

    couch

--- 005/main.js
+++ 005/main.js
@@ -5,44 +5,29 @@ window.onload = function(){
   var context = canvas.getContext('2d');
 
 
-    var particlesVelocity = [];
-    var particles = [];
+  var angle = 0;
+  var speed = 0.1;
+  var radius = 100;
 
-  var restart = function(){
-    particlesVelocity = [];
-    particles = [];
-    for(var ii = 0; ii < 20; ii++){
-      particles.push( vector.create(0, 0));
-      var v1 =  vector.create(Math.random() - 0.5, Math.random()  - 0.5);
-      particlesVelocity.push(v1);
-    }
 
-      context.clearRect(0,0, width, height);
 
-  }
 
-  document.addEventListener('mousedown', function(){
-    console.log('click');
-    restart();
-  });
-  restart();
-    var render = function(){
+  var render = function(){
       context.save();
       context.translate(width/2, height/2);
 
-      for(var ii = 0; ii < particles.length; ii++){
-        var vel = particlesVelocity[ii];
-        var particle = particles[ii];
-        particle.addTo(vel);
+
+      var x = Math.cos(angle) * radius;
+      var y = Math.sin(angle) * radius;
+
+      angle += speed;
+      console.log('x', x, 'y', y);
+
+      context.arc(x,y, 20, 0, Math.PI * 2);
+      context.stroke();
 
 
-        context.strokeStyle = "#fcec59";
-        context.strokeStyle = "#faff1d";
-        context.beginPath();
-        context.arc(particle.getX(),particle.getY(),50,0,2*Math.PI);
-        context.stroke();
-        // Reseet
-      }
+
 
       context.restore();
 
