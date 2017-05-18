48985515c9a5909020e277f0f27836d1d0f30f91
jason <bjason@gmail.com>
2017-05-17 23:00:03 -0700

    clear react

--- 005/main.js
+++ 005/main.js
@@ -4,33 +4,32 @@ window.onload = function(){
   var height = canvas.height = window.innerHeight;
   var context = canvas.getContext('2d');
 
-
   var angle = 0;
   var speed = 0.1;
   var radius = 100;
 
-
-
+  var restart = function(){
+    console.log('mousedown');
+    context.clearRect(0, 0, width, height);
+  };
+  document.addEventListener('mousedown', function(){
+    restart();
+  });
 
   var render = function(){
       context.save();
       context.translate(width/2, height/2);
 
-
       var x = Math.cos(angle) * radius;
       var y = Math.sin(angle) * radius;
 
       angle += speed;
-      console.log('x', x, 'y', y);
+      // console.log('x', x, 'y', y);
 
+      context.beginPath();
       context.arc(x,y, 20, 0, Math.PI * 2);
       context.stroke();
-
-
-
-
       context.restore();
-
       requestAnimationFrame(render, 1000/60);
     }
 
