672adfab47389a4fc4f0d970cf3949695eb26b96
jason <bjason@gmail.com>
2017-05-20 13:10:39 -0700

    Day 8: project 8- a smiple game

---
+++ 008/sketch.js
@@ -0,0 +1,24 @@
+var width;
+var height;
+
+function preload(){
+}
+
+function setup(){
+  width = window.innerWidth;
+  height = window.innerHeight;
+  console.log(width);
+  console.log(height);
+  createCanvas(width, height);
+}
+
+function draw(){
+  // ellipse(50,50, 80, 80);
+  if (mouseIsPressed){
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  ellipse(mouseX, mouseY, 80, 80);
+}
