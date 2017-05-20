05e4ead0694f0b983c0fb3b01a1ae1c7230e2a9b
jason <bjason@gmail.com>
2017-05-20 13:36:41 -0700

    Simple little bees

--- 008/sketch.js
+++ 008/sketch.js
@@ -1,15 +1,34 @@
 var width;
 var height;
 var bees = [];
+var points = 0;
+var beesCount = 0;
+
+// Example bee:
 var sampleBee = {
   x: 0,
   y: 0,
-  speed: 0.1,
+  width: 10,
+  xSpeed: 0.1,
+  ySpeed:  0.5,
+  isQueen: false,
   radius: 100
 };
 
 function preload(){
-  
+  for(var ii = 0; beesCount; ii++){
+    var bee = {
+      x: 0,
+      y: 0,
+      width: 10,
+      xSpeed: random(-10, 10) / 10 ,
+      ySpeed:  random(-5, 5) * 3 ,
+      isQueen: false,
+      radius: 100
+    };
+  }
+  bees[bees.length -1 ].isQueen = true;
+  print(bees):
 }
 
 function setup(){
@@ -21,10 +40,9 @@ function setup(){
 }
 
 function draw(){
-  // ellipse(50,50, 80, 80);
   clear();
-
-  fill(0);
+  fill(0); // set to black 
+  text("Poinst: " + points, 10, 10);
   ellipse(200, 200, 100);
 
   if (mouseIsPressed){
@@ -32,10 +50,7 @@ function draw(){
   } else {
     fill(255);
   }
-
   ellipse(mouseX, mouseY, 80, 80);
-
-  hit = collideCircleCircle(mouseX,mouseY,150,200,200,100)
+  var hit = collideCircleCircle(mouseX,mouseY,150,200,200,100)
 	print("colliding? " + hit);
-
 }
