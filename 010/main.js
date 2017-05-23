3f51a93ef3d7423627f36e40dab377f0b70ef534
jason <bjason@gmail.com>
2017-05-22 20:38:47 -0700

    lil ship

--- 010/main.js
+++ 010/main.js
@@ -4,8 +4,27 @@ function loaded(){
   var width = canvas.width = window.innerWidth;
   var height = canvas.height = window.innerHeight;
 
+  // Params to rotate in a circle;
+  var drawShip = function(){
+    context.beginPath();
+    context.moveTo(15,0);
+    // Draw top part of ship
+    context.lineTo(-15, -20);
+    context.stroke();
+    context.moveTo(15,0);
+    context.lineTo(-15, 20);
+    context.stroke();
+    context.moveTo(-15,-20);
+    context.lineTo(-15,20);
+    context.stroke();
+  }
   var render = function(){
-    context.fillRect(20,20,100,100);
+    context.restore();
+    context.save();
+    context.translate(width/2, height/2);
+    drawShip();
+
+
     setTimeout(function(){
       requestAnimationFrame(render);
     }, 1000/60);
