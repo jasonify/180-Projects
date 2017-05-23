a8663fa1221e6d20681a317613569bfe4e3fa50f
jason <bjason@gmail.com>
2017-05-22 20:45:35 -0700

    drawing little ship

--- 010/main.js
+++ 010/main.js
@@ -3,6 +3,22 @@ function loaded(){
   var context = canvas.getContext('2d'); 
   var width = canvas.width = window.innerWidth;
   var height = canvas.height = window.innerHeight;
+  var mouseX = 0;
+  var mouseY = 0;
+
+  document.addEventListener('mousemove', function(event){
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  });
+
+  var getRotationPositionToCenter = function(x,y){
+    var centerX = width/2;
+    var centerY = height/2;
+    var dx = centerX - x;
+    var dy = centerY - y ;
+    var angle = Math.atan2(dx, dy);
+    return angle;
+  }
 
   // Params to rotate in a circle;
   var drawShip = function(){
@@ -21,7 +37,9 @@ function loaded(){
   var render = function(){
     context.restore();
     context.save();
+    context.clearRect(0, 0, width, height);
     context.translate(width/2, height/2);
+    // context.rotate(getRotationPositionToCenter(mouseX, mouseY));
     drawShip();
 
 
